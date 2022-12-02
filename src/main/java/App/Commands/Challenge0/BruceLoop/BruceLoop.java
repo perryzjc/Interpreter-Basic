9ab3fea241a0debe20e8045645268b17eedd6a4c
Jingchao Zhong <perry.zjc@gmail.com>
2022-12-02 13:01:11 -0800

    3 ~ 16, impossible

--- src/main/java/App/Commands/Challenge0/BruceLoop/BruceLoop.java
+++ src/main/java/App/Commands/Challenge0/BruceLoop/BruceLoop.java
@@ -27,8 +27,8 @@ public class BruceLoop {
     private int currIndex;
 
     public static void main(String[] args) {
-        //finished: 3， 4， 5， 6， 7， 8， 9， 10， 11， 12， 13, 14, 15
-        for (int i = 15; i < 20; i++) {
+        //finished: 3， 4， 5， 6， 7， 8， 9， 10， 11， 12， 13, 14, 15, 16
+        for (int i = 16; i < 20; i++) {
             BruceLoop bruceLoop = new BruceLoop(i);
 //            BruceLoop bruceLoop = new BruceLoop(SEBASTIAN_SOL_NUM_COMMANDS);
             bruceLoop.startForLoop();
