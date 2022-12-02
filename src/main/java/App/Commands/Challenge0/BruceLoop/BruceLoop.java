b9851a5a4cc8a3b9588427b9e75c8cf19091dfec
Jingchao Zhong <perry.zjc@gmail.com>
2022-12-02 11:57:32 -0800

    fix negative problem and now the BruceLoop can work when the correct answer provided such as sebastian's solution

--- src/main/java/App/Commands/Challenge0/BruceLoop/BruceLoop.java
+++ src/main/java/App/Commands/Challenge0/BruceLoop/BruceLoop.java
@@ -1,6 +1,7 @@
 package App.Commands.Challenge0.BruceLoop;
 
 import App.Commands.Basic.Command;
+import App.Commands.Challenge0.CmdXORSebastian;
 import App.Commands.CmdHelper;
 import App.Commands.DefinedCmd.DefinedCmd;
 import App.MemorySpace;
@@ -10,9 +11,9 @@ import App.Store;
 import java.util.ArrayList;
 
 public class BruceLoop {
+    private final static int SEBASTIAN_SOL_NUM_COMMANDS = 32;
     private final ArrayList<Command> usableCommands = new ArrayList<>();
     private final ArrayList<Integer> currCombination = new ArrayList<>();
-    private final ArrayList<Command> result = new ArrayList<>();
     private final int NUM_OPTIONS_CMD = 4;
 
     private int _max_commands_used;
@@ -20,11 +21,16 @@ public class BruceLoop {
     private MemorySpace memorySpace;
     private Store store;
     private CmdHelper cmdHelper;
+    private CurrDefinedCmd currDefinedCmd;
+    private ArrayList<Command> result = new ArrayList<>();
+    /* curr index to put the command in result */
+    private int currIndex;
 
     public static void main(String[] args) {
         //finished: 3， 4， 5， 6， 7， 8， 9， 10， 11， 12， 13， 14， 15， 16
-        for (int i = 3; i < 28; i++) {
-            BruceLoop bruceLoop = new BruceLoop(i);
+        for (int i = 27; i < 28; i++) {
+            //BruceLoop bruceLoop = new BruceLoop(i);
+            BruceLoop bruceLoop = new BruceLoop(SEBASTIAN_SOL_NUM_COMMANDS);
             bruceLoop.startForLoop();
             System.out.println("finished: " + i);
         }
@@ -90,13 +96,6 @@ public class BruceLoop {
         }
     }
 
-    private void loadCommandsBasedOnCombinationArray() {
-        for (int i = 0; i < _max_commands_used; i++) {
-            Command cmd = usableCommands.get(currCombination.get(i));
-            result.set(i, cmd);
-        }
-    }
-
     public void startForLoop() {
         long loopTimes = (long) Math.pow(NUM_OPTIONS_CMD, _max_commands_used);
         for (long i = 0; i < loopTimes; i++) {
@@ -105,14 +104,15 @@ public class BruceLoop {
             if (i != 0) {
                 nextCombination();
             }
-            loadCommandsBasedOnCombinationArray();
+            currDefinedCmd = new CurrDefinedCmd(pointer, memorySpace, store);
             if (!test000() || !test011() || !test101() || !test110()) {
                 continue;
             } else {
+                loadToResult(currDefinedCmd);
                 for (int j = 0; j < _max_commands_used; j++) {
                     System.out.println(result.get(j).commandName());
                 }
-                System.out.println("Found a solution!");
+                System.out.println("Found a solution! Number of commands used: " + result.size());
                 return;
             }
         }
@@ -142,37 +142,59 @@ public class BruceLoop {
     }
 
     private ArrayList<Integer> getSampleInitMemorySpaceForFirstTwoPos(int int1, int int2) {
-        ArrayList<Integer> result = new ArrayList<>();
-        result.add(int1);
-        result.add(int2);
+        ArrayList<Integer> sample = new ArrayList<>();
+        sample.add(int1);
+        sample.add(int2);
         for (int i = 2; i < _max_commands_used; i++) {
-            result.add(0);
+            sample.add(0);
         }
-        return result;
+        return sample;
     }
 
     private boolean test000() {
         resetState(INIT_STATE.T00);
-        new CurrDefinedCmd(pointer, memorySpace, store).execute();
+        currDefinedCmd.execute();
         return memorySpace.getBitForTestOnly(2) == 0;
     }
 
     private boolean test011() {
         resetState(INIT_STATE.T01);
-        new CurrDefinedCmd(pointer, memorySpace, store).execute();
+        currDefinedCmd.execute();
         return memorySpace.getBitForTestOnly(2) == 1;
     }
 
     private boolean test101() {
         resetState(INIT_STATE.T10);
-        new CurrDefinedCmd(pointer, memorySpace, store).execute();
+        currDefinedCmd.execute();
         return memorySpace.getBitForTestOnly(2) == 1;
     }
 
     private boolean test110() {
         resetState(INIT_STATE.T11);
-        new CurrDefinedCmd(pointer, memorySpace, store).execute();
+        currDefinedCmd.execute();
         return memorySpace.getBitForTestOnly(2) == 0;
+    }
+
+    /**
+     * load every command in cmdList of definedCmd to result list
+     * @param definedCmd
+     */
+    private void loadToResult(DefinedCmd definedCmd) {
+        currIndex = 0;
+        loadToResultHelper(definedCmd);
+    }
+
+    private void loadToResultHelper(DefinedCmd definedCmd) {
+        ArrayList<Command> cmdList = definedCmd.getCmdList();
+        for (int i = 0; i < cmdList.size(); i++) {
+            Command command = cmdList.get(i);
+            if (command instanceof DefinedCmd) {
+                loadToResultHelper((DefinedCmd) command);
+            } else {
+                result.set(currIndex, command);
+                currIndex++;
+            }
+        }
     }
 
     private class CurrDefinedCmd extends DefinedCmd {
@@ -183,9 +205,18 @@ public class BruceLoop {
 
         @Override
         protected void loadCommands() {
+            testCorrectCmdSebastianSolution();
+        }
+
+        private void testCorrectCmdSebastianSolution() {
+            CmdXORSebastian cmdXORSebastian = new CmdXORSebastian(pointer, memorySpace, store);
+            cmdList.add(cmdXORSebastian);
+        }
+
+        private void loadCommandsBasedOnCombinationArray() {
             for (int i = 0; i < _max_commands_used; i++) {
-                Command cmd = result.get(i);
-                cmdList.add(cmd);
+                Command cmd = usableCommands.get(currCombination.get(i));
+                result.set(i, cmd);
             }
         }
     }
