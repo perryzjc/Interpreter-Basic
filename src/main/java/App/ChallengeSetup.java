02f1a8b9b98d876e230ed0ce035d5cbe87b2c786
Jingchao Zhong <perry.zjc@gmail.com>
2022-12-04 02:38:14 -0800

    add my thinking of potential solution to challenge 2&3 at ChallengeSetup.java file

--- src/main/java/App/ChallengeSetup.java
+++ src/main/java/App/ChallengeSetup.java
@@ -63,6 +63,17 @@ public class ChallengeSetup {
      * input A at 0-15 and B at 16-31
      * based on discrete math
      * there are 2^16 * 2^16 = 2^32 possible initial state
+     * 4294967296 is an extremely large number, my computer is unable to handle such many branches at the same time
+     * If I want to try, I at least need replace each object,
+     * since an empty object already cost 16bits
+     * TODO: may become up with a equivalent way to simulates calculation to BitSet object, which use less memory
+     *
+     * TODO: for the time being, I think for challenge 2&3 I need to set up an init branch limit
+     * just like the local test provided by the official, it only tests 100 different input files
+     *
+     * TODO: to verify the absolute correctness of the solution, I should come up with a way to test all possible initial state,
+     * TODO: one of way is separate the init branches into different files, and handle each of them in different stage to avoid heap memory limit
+     *
      */
     public ArrayList<Branch> generateInitBranchList() {
         return null;
