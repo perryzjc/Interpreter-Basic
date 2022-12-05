c6a76913a68e29134f9d0f7b1af9f480d6fdc9eb
Jingchao Zhong <perry.zjc@gmail.com>
2022-12-05 01:50:55 -0800

    trying new strategy for moving

--- src/main/java/App/Commands/Strategy/ExtraStrategyForMov.java
+++ src/main/java/App/Commands/Strategy/ExtraStrategyForMov.java
@@ -1,2 +1,30 @@
-package App.Commands.Strategy;public class ExtraStrategyForMov {
-}
+//package App.Commands.Strategy;
+//
+//public class ExtraStrategyForMov {
+//    private int curr_pointer;
+//
+//    private class CurrMeaningfulStatus {
+//        private boolean INV;
+//        private boolean LOAD;
+//        private boolean INC;
+//        private boolean CDEC;
+//
+//        public CurrMeaningfulStatus() {
+//            storeCurrStatus();
+//        }
+//
+//        private void storeCurrStatus() {
+//            INV = INVMeaningful;
+//            LOAD = LOADMeaningful;
+//            INC = INCMeaningful;
+//            CDEC = CDECMeaningful;
+//        }
+//
+//        private void recoverStatus() {
+//            INVMeaningful = INV;
+//            LOADMeaningful = LOAD;
+//            INCMeaningful = INC;
+//            CDECMeaningful = CDEC;
+//        }
+//    }
+//}
